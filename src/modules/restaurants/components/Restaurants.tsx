9025acfdf4057f3777396f642923c5cc9f9f2f3e
KarinMoveo <karino@moveo.co.il>
2023-08-22 17:10:09 +0300

    Working on the restaurant page

--- src/modules/restaurants/components/Restaurants.tsx
+++ src/modules/restaurants/components/Restaurants.tsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
 import "../components/Restaurants.scss";
 
 function Restaurants() {
+    const [selectedCategoryItem, setSelectedCategoryItem] = useState("All"); // Initialize with "All"
+
+    const handleCategoryClick = (category:string) => {
+        setSelectedCategoryItem(category);
+    };
+
     return (
-        <div className="restaurants-page-container">Restaurants Page Content</div>
+        <div className="restaurants-page-container">
+            <p className="restaurant-page-title">RESTAURANTS</p>
+            <div className="restaurants-categories-container">
+                <p
+                    className={`restaurant-category-item ${
+                        selectedCategoryItem === "All" ? "selected" : ""
+                    }`}
+                    onClick={() => handleCategoryClick("All")}
+                >
+                    All
+                </p>
+                <p
+                    className={`restaurant-category-item ${
+                        selectedCategoryItem === "New" ? "selected" : ""
+                    }`}
+                    onClick={() => handleCategoryClick("New")}
+                >
+                    New
+                </p>
+                <p
+                    className={`restaurant-category-item ${
+                        selectedCategoryItem === "Most Popular" ? "selected" : ""
+                    }`}
+                    onClick={() => handleCategoryClick("Most Popular")}
+                >
+                    Most Popular
+                </p>
+                <p
+                    className={`restaurant-category-item ${
+                        selectedCategoryItem === "Open Now" ? "selected" : ""
+                    }`}
+                    onClick={() => handleCategoryClick("Open Now")}
+                >
+                    Open Now
+                </p>
+            </div>
+        </div>
     );
-  }
-  
-  export default Restaurants;
+}
+
+export default Restaurants;
