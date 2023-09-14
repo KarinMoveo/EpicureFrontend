6a4d066730ed6dba587f4e04ddacfef5dd264875
KarinMoveo <karino@moveo.co.il>
2023-09-14 11:57:59 +0300

    Ui changes

--- src/shared/components/ChefImageAndName.tsx
+++ src/shared/components/ChefImageAndName.tsx
@@ -3,7 +3,6 @@ import './ChefImageAndName.scss';
 function ChefImageAndName(props: any) {
 	return (
 		<div className='image-overlay-container'>
-			<button onClick={props.onDelete}>Delete</button>
 			<img src={props.chefImage} alt={props.chefName} className='chef-of-the-week-image' />
 			<div className='overlay-name'>{props.chefName}</div>
 		</div>
