5d02c06ab8dce370ee70cc05098a94dc02deb728
KarinMoveo <karino@moveo.co.il>
2023-09-14 09:08:48 +0300

    Working on CRUD - add chef option

--- src/modules/chefs/api.ts
+++ src/modules/chefs/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { chef } from "../../mockData/data/types";
 
 export async function getAllChefsFromAPI(selectedCategoryItem : any){
     const result = await axios.get(
@@ -11,6 +12,14 @@ export async function getAllChefsFromAPI(selectedCategoryItem : any){
 export async function deleteChefFromAPI(id: any){
     const result = await axios.get(
         `http://localhost:5000/chefs/deleteChef/${id}`
+    );
+    
+    return result;
+}
+
+export async function addChefFromAPI(name: string, image: string){
+    const result = await axios.get(
+        `http://localhost:5000/chefs/addChef?name=${name}&image=${image}`
     );
     
     return result;
--- src/modules/chefs/components/Chefs.tsx
+++ src/modules/chefs/components/Chefs.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
 import ChefImageAndName from '../../../shared/components/ChefImageAndName';
+import { addChefFromAPI, deleteChefFromAPI, getAllChefsFromAPI } from '../api';
 import '../components/Chefs.scss';
-import { deleteChefFromAPI, getAllChefsFromAPI } from '../api';
 
 type selectedCategoryType = 'All' | 'New' | 'Most Viewed' | null;
 const categories: selectedCategoryType[] = ['All', 'New', 'Most Viewed'];
 
 function Chefs() {
 	const [allChefs, setAllChefs] = useState([]);
-
 	const [selectedCategoryItem, setSelectedCategoryItem] = useState<selectedCategoryType>('All');
+	const [newChef, setNewChef] = useState({
+		name: '',
+		image: '',
+	});
 
 	const handleCategoryClick = (category: selectedCategoryType) => {
 		setSelectedCategoryItem(category);
@@ -28,10 +31,17 @@ function Chefs() {
 		getAllChefs();
 	}, [selectedCategoryItem]);
 
-	const handleOnDeleteChef = async (id: number) => {
+	async function handleOnDeleteChef(id: number) {
 		await deleteChefFromAPI(id);
 		await getAllChefs();
-	};
+	}
+
+	async function handleOnAddChefClick() {
+		const { name, image } = newChef;
+		await addChefFromAPI(name, image);
+		setNewChef({ name: '', image: '' });
+		await getAllChefs();
+	}
 
 	return (
 		<div className='chefs-page-container'>
@@ -58,6 +68,24 @@ function Chefs() {
 						onDelete={() => handleOnDeleteChef(chef.id)}
 					/>
 				))}
+			</div>
+			<div className='add-chefs-container'>
+				<p>ADD CHEF!</p>
+				<label>Chef Name: </label>
+				<input
+					type='text'
+					value={newChef.name}
+					onChange={(e) => setNewChef({ ...newChef, name: e.target.value })}
+					required
+				/>
+				<label>Chef Image: </label>
+				<input
+					type='text'
+					value={newChef.image}
+					onChange={(e) => setNewChef({ ...newChef, image: e.target.value })}
+					required
+				/>
+				<button onClick={() => handleOnAddChefClick()}>ADD</button>
 			</div>
 		</div>
 	);
