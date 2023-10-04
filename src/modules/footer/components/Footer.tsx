d35fee8ebcce58defd5ca454dca10c7f1b93942f
KarinMoveo <karino@moveo.co.il>
2023-10-04 11:57:13 +0300

    More changes

--- src/modules/footer/components/Footer.tsx
+++ src/modules/footer/components/Footer.tsx
@@ -1,20 +1,12 @@
-import { Link } from 'react-router-dom';
-
 import '../components/Footer.scss';
 
 function Footer() {
 	return (
 		<footer className='footer-container'>
 			<ul className='ul-footer-container'>
-				<li className='footer-item'>
-					<Link to='/contact'>Contact Us</Link>
-				</li>
-				<li className='footer-item'>
-					<Link to='/terms'>Term Of Use</Link>
-				</li>
-				<li className='footer-item'>
-					<Link to='/privacy'>Privacy Policy</Link>
-				</li>
+				<li className='footer-item'>Contact Us</li>
+				<li className='footer-item'>Term Of Use</li>
+				<li className='footer-item'>Privacy Policy</li>
 			</ul>
 		</footer>
 	);
